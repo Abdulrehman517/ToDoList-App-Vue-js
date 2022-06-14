25096fc5ee077ac8667c76287b640b94638f05bd
Abdulrehman <72541146+Abdulrehman517@users.noreply.github.com>
2022-06-14 16:00:40 +0500

    Add files via upload
    to do list in vue js

---
+++ src/components/child.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div>
+    <button v-on:click="updateTitle"> update title</button>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Child-vue',
+    methods: {
+        updateTitle() {
+            this.$emit('changeTitle', 'Props has been Updated!!!')
+        }
+    }
+
+
+}
+</script>
